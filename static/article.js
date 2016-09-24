2cb52e15a3d27098a7cba414646a1438426ce49f
Émilien Tlapale <emilien@tlapale.com>
2016-09-24 22:43:26 +0200

    Reformatting keywords.

--- static/article.js
+++ static/article.js
@@ -7,25 +7,38 @@ function styleMetadata() {
     // Search the authors in the metadata
     var fieldList = document.querySelector('table.field-list');
     var fields = fieldList.querySelectorAll('table.field-list tr.field');
-    var authors;
+    var authors, keywords;
     for (var field of fields) {
 	var fieldName = field.querySelector('th').textContent;
 	var fieldValue = field.querySelector('td').textContent;
 
 	if (fieldName == "Authors:")
 	    authors = fieldValue;
+	else if (fieldName == "Keywords:")
+	    keywords = fieldValue;
     }
 
-    // Reformat the authors list
+    // Reformat the field lists
     authors = authors.replace(/\s*;\s*/g, ' · ');
+    keywords = keywords.replace(/\s*;\s*/g, ' · ');
 
     // Insert a new authors list
     var authorsDiv = document.createElement('div');
     authorsDiv.className = 'article-authors';
-    authorsDiv.className.style = 'margin-top: -1ex;';
     authorsDiv.appendChild(document.createTextNode(authors));
     fieldList.parentNode.insertBefore(authorsDiv, fieldList);
 
+    // Insert the keywords in the abstract
+    var abstr = document.querySelector("div.abstract");
+    var kwdsDiv = document.createElement('div');
+    kwdsDiv.className = 'article-keywords';
+    var kwdsLabel = document.createElement('strong');
+    kwdsLabel.appendChild(document.createTextNode('Keywords:'));
+    kwdsDiv.appendChild(kwdsLabel);
+    kwdsDiv.appendChild(document.createTextNode(' ' + keywords));
+    abstr.appendChild(kwdsDiv);
+
+    // Hide the raw field list
     fieldList.style.display = 'none';
 }
 
